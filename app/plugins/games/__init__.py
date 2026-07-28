bcfc1bc6ff1f9fe2996f3769d973e6c05dd218b4
ch4t9pt <kt19950321@gmail.com>
2026-07-29 08:27:28 +0900

    Games PluginのFactory公開口を追加

--- app/plugins/games/__init__.py
+++ app/plugins/games/__init__.py
@@ -1,3 +1,4 @@
+from app.plugins.games.factory import GamesPluginFactory, plugin_factory
 from app.plugins.games.plugin import GamesPlugin
 
-__all__ = ["GamesPlugin"]
+__all__ = ["GamesPlugin", "GamesPluginFactory", "plugin_factory"]
