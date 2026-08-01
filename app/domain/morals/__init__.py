a942816a1d698abcc96fefa308c32be595cb3df3
ch4t9pt <kt19950321@gmail.com>
2026-08-01 18:18:53 +0900

    Moralドメイン型を公開

---
+++ app/domain/morals/__init__.py
@@ -0,0 +1,11 @@
+from app.domain.morals.moral_state import (
+    MoralComposite,
+    MoralProfile,
+    MoralState,
+)
+
+__all__ = [
+    "MoralComposite",
+    "MoralProfile",
+    "MoralState",
+]
