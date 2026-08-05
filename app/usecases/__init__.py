57351510f2caadc4aa7cd499cc57908fccf048a0
ch4t9pt <kt19950321@gmail.com>
2026-08-06 06:36:08 +0900

    Motion対応UseCaseを公開

--- app/usecases/__init__.py
+++ app/usecases/__init__.py
@@ -1,7 +1,7 @@
-from app.usecases.avatar_aware_execute_action_usecase import ExecuteActionUsecase
 from app.usecases.delivery_aware_execute_action_usecase import (
     ExecuteActionUsecase as DeliveryAwareExecuteActionUsecase,
 )
+from app.usecases.motion_aware_execute_action_usecase import ExecuteActionUsecase
 
 __all__ = [
     "DeliveryAwareExecuteActionUsecase",
