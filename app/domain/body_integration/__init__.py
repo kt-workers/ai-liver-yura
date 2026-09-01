af00c01cb9e0288ac753dbcf9a922069db0599c3
ch4t9pt <kt19950321@gmail.com>
2026-09-02 03:05:18 +0900

    実装: #341 integration runtimeを公開する

--- app/domain/body_integration/__init__.py
+++ app/domain/body_integration/__init__.py
@@ -1,7 +1,11 @@
 from .contracts import BodyExecutionSession, BodyExecutionSessionStatus, BodyIntegrationTrace
+from .runtime import BodyIntegrationRuntime, BodyMotionPlanningPort, BodyPlanningSubmission
 
 __all__ = [
     "BodyExecutionSession",
     "BodyExecutionSessionStatus",
+    "BodyIntegrationRuntime",
     "BodyIntegrationTrace",
+    "BodyMotionPlanningPort",
+    "BodyPlanningSubmission",
 ]
